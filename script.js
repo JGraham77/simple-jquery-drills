600a672625d0243b0d6042bb4f5a1c4a57e2938c
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-02-09 17:43:20 -0600

    complete lab task 7

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+const $txtSubmit = $(`#txtSubmit`);
+
+const $btnSubmit = $(`#btnSubmit`);
+
+
 //Select input with jQuery, on click add alert
-$(`#btnSubmit`).click(function () {
-    alert($(`#txtSubmit`).val());
+$btnSubmit.click(function () {
+    alert($txtSubmit.val());
+});
+
+$txtSubmit.keyup(() => {
+    if ($txtSubmit.val() !== '') {
+        // Allows the button to be used
+        $btnSubmit.attr('disabled', false);
+    }
 });
