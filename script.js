f2d00490ee1f004e9fe760735ddc6066fa2074c9
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-02-10 11:24:35 -0600

    complete lab

--- script.js
+++ script.js
@@ -20,7 +20,47 @@ $txtSubmit.keyup(() => {
 const $div = $(`<div class='container'></div>`);
 $div.appendTo('body');
 
+// $btnSubmit.click(() => {
+//     $div.append(`<h2>${$txtSubmit.val()}</h2>`);
+//     $txtSubmit.val('');
+// });
+
+// // Get random color
+// const vals = "0123456789ABCDEF";
+// let colorString = "#";
+// for (let i = 0; i < 6; i++) {
+//     const char = vals[Math.floor(Math.random() * vals.length)];
+//     colorString += char
+// };
+
+
+// // Mouseover h2 to change color
+// $('h2').mouseover(function () {
+//     const thisColorString = colorString();
+//     $('h2').css({
+//         'background-color': thisColorString,
+//         'border-radius': '0.25rem'
+//     });
+// });
+
+const $ul = $(`<ul></ul>`);
+$ul.appendTo('body');
+
 $btnSubmit.click(() => {
-    $div.append(`<h2>${$txtSubmit.val()}</h2>`);
-    $txtSubmit.val('');
-})
+    $ul.append(`<li>${$txtSubmit.val()}</li>`)
+
+    const vals = "0123456789ABCDEF";
+    let colorString = "#";
+    for (let i = 0; i < 6; i++) {
+        let char = vals[Math.floor(Math.random() * vals.length)];
+        colorString += char
+    };
+
+    $('li').click(function () {
+        $('li').css('color', colorString);
+    });
+
+    $('li').dblclick(function () {
+        $(event.target).remove();
+    });
+});
